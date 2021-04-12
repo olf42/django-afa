040d177b6a39287ae3f46ddae0dbfcf2b80e175b
Florian Rämisch <olf@subsignal.org>
2021-04-12 23:12:01 +0200

    Add deduction method + tests

--- afa/models.py
+++ afa/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from djmoney.models.fields import MoneyField
 
 
+class AfaYearError(Exception):
+    pass
+
+
 class Afa(models.Model):
     title = models.CharField(max_length=255)
     useful_life = models.PositiveIntegerField()
@@ -15,3 +19,21 @@ class Entity(models.Model):
     date_of_purchase = models.DateField()
     price = MoneyField(max_digits=14, decimal_places=2, default_currency='EUR')
     afa_type = models.ForeignKey("Afa", on_delete=models.PROTECT)
+
+    def deduction(self, year: int):
+        month_of_purchase = self.date_of_purchase.month
+        useful_life = self.afa_type.useful_life
+        months_1st_year = 12 - month_of_purchase + 1
+        months_last_year = 12 - months_1st_year
+        if year < self.date_of_purchase.year:
+            raise AfaYearError("Deduction is only allowed in or after the year of purchase.")
+        if (year - self.date_of_purchase.year) == 0:
+            return (months_1st_year/(12*useful_life))*self.price
+        elif (year - self.date_of_purchase.year) < useful_life:
+            return 1/useful_life*self.price
+        else:
+            return (months_last_year/(12*useful_life))*self.price
+
+    @property
+    def deduction_plan(self):
+        pass
--- afa/tests/tests.py
+++ afa/tests/tests.py
@@ -1,12 +1,17 @@
 from datetime import date
 from django.test import TestCase
-from afa.models import Afa, Entity
+from afa.models import Afa, Entity, AfaYearError
 from afa.afa2json import Afa2Json
 from djmoney.money import Money
 
 BIERZELT_ID = 'Zelte, Bier- '
+BIERZELT_NAME = "Unser Bierzelt"
 
-class AfaTestCase(TestCase):
+
+class AfaCreateCase(TestCase):
+    """
+    Tests the initial data fetching and object creation.
+    """
 
     @classmethod
     def setUpTestData(cls):
@@ -22,10 +27,53 @@ class AfaTestCase(TestCase):
 
     def test_entity_creation(self):
         bierzelt_type = Afa.objects.get(title=BIERZELT_ID)
-        e = Entity.objects.create(
-            name="Unser Bierzelt",
-            price=Money(12345.67, "EUR"),
+        ub = Entity.objects.create(
+            name=BIERZELT_NAME,
+            price=Money(9600.00, "EUR"),
             date_of_purchase=date(year=2021, month=4, day=21),
             afa_type=bierzelt_type
         )
-        e.save()
+        ub.save()
+
+
+class AfaUseCase(TestCase):
+    """
+    Tests if the cool calculates correctly.
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        for entry in Afa2Json.from_url().data:
+            if not entry["useful_life"]:
+                entry["useful_life"] = 0
+            a = Afa.objects.create(
+                title=entry["title"],
+                useful_life=entry["useful_life"],
+                source=entry["source"]
+            )
+            a.save()
+        bierzelt_type = Afa.objects.get(title=BIERZELT_ID)
+        ub = Entity.objects.create(
+            name=BIERZELT_NAME,
+            price=Money(9600.00, "EUR"),
+            date_of_purchase=date(year=2021, month=4, day=21),
+            afa_type=bierzelt_type
+        )
+        ub.save()
+
+    def test_deduction_raises(self):
+        nb = Entity.objects.get(name=BIERZELT_NAME)
+        self.assertRaises(AfaYearError, nb.deduction, 2020)
+
+    def test_deduction_first_year(self):
+        nb = Entity.objects.get(name=BIERZELT_NAME)
+        self.assertEqual(nb.deduction(2021), Money(900.00, "EUR"))
+
+    def test_deduction_inbetween_year(self):
+        nb = Entity.objects.get(name=BIERZELT_NAME)
+        for year in range(2022, 2029):
+            self.assertEqual(nb.deduction(year), Money(1200.00, "EUR"))
+
+    def test_deduction_last_year(self):
+        nb = Entity.objects.get(name=BIERZELT_NAME)
+        self.assertEqual(nb.deduction(2029), Money(300.00, "EUR"))
